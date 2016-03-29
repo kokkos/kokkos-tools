b568ff4607ef8e250b595e076f29ae38526aacf9
crtrott <crtrott@sandia.gov>
2016-03-29 11:34:50 -0600

    NVPROF: add focused connector.

---
+++ src/tools/nvprof-focused-connector/kp_nvprof_focused_connector_domain.h
@@ -0,0 +1,74 @@
+
+#ifndef _H_KOKKOSP_KERNEL_NVPROF_CONNECTOR_INFO
+#define _H_KOKKOSP_KERNEL_NVPROF_CONNECTOR_INFO
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <cstring>
+
+#include "nvToolsExt.h"
+
+enum KernelExecutionType {
+	PARALLEL_FOR = 0,
+	PARALLEL_REDUCE = 1,
+	PARALLEL_SCAN = 2
+};
+
+class KernelNVProfFocusedConnectorInfo {
+	public:
+		KernelNVProfFocusedConnectorInfo(std::string kName, KernelExecutionType kernelType) {
+
+		  domainNameHandle = kName;
+			char* domainName = (char*) malloc( sizeof(char*) * (32 + kName.size()) );
+
+			if(kernelType == PARALLEL_FOR) {
+				sprintf(domainName, "ParallelFor.%s", kName.c_str());
+			} else if(kernelType == PARALLEL_REDUCE) {
+				sprintf(domainName, "ParallelReduce.%s", kName.c_str());
+			} else if(kernelType == PARALLEL_SCAN) {
+				sprintf(domainName, "ParallelScan.%s", kName.c_str());
+			} else {
+				sprintf(domainName, "Kernel.%s", kName.c_str());
+			}
+
+			domain = nvtxDomainCreateA(domainName);
+			currentRange = 0;
+		}
+
+		nvtxRangeId_t startRange() {
+		  nvtxEventAttributes_t eventAttrib = {0};
+		  eventAttrib.version = NVTX_VERSION;
+		  eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+		  eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
+		  eventAttrib.message.ascii = "my range";
+		  currentRange = nvtxDomainRangeStartEx(domain,&eventAttrib);
+		  return currentRange;
+		}
+
+		nvtxRangeId_t getCurrentRange() {
+		  return currentRange;
+		}
+
+		void endRange() {
+		  nvtxDomainRangeEnd(domain,currentRange);
+		}
+
+		nvtxDomainHandle_t getDomain() {
+			return domain;
+		}
+
+		std::string getDomainNameHandle() {
+			return domainNameHandle;
+		}
+
+		~KernelNVProfFocusedConnectorInfo() {
+		  nvtxDomainDestroy(domain);
+		}
+
+	private:
+		std::string domainNameHandle;
+		nvtxRangeId_t currentRange;
+		nvtxDomainHandle_t domain;
+};
+
+#endif
