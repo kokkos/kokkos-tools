e214364ae1c08677b414e767c57ac4965510c684
David Poliakoff <david.poliakoff@gmail.com>
2019-10-17 13:04:04 -0600

    Added include files to repo

---
+++ include/impl/Kokkos_Profiling_C_Interface.h
@@ -0,0 +1,156 @@
+/*
+ //@HEADER
+ // ************************************************************************
+ //
+ //                        Kokkos v. 2.0
+ //              Copyright (2014) Sandia Corporation
+ //
+ // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+ // the U.S. Government retains certain rights in this software.
+ //
+ // Redistribution and use in source and binary forms, with or without
+ // modification, are permitted provided that the following conditions are
+ // met:
+ //
+ // 1. Redistributions of source code must retain the above copyright
+ // notice, this list of conditions and the following disclaimer.
+ //
+ // 2. Redistributions in binary form must reproduce the above copyright
+ // notice, this list of conditions and the following disclaimer in the
+ // documentation and/or other materials provided with the distribution.
+ //
+ // 3. Neither the name of the Corporation nor the names of the
+ // contributors may be used to endorse or promote products derived from
+ // this software without specific prior written permission.
+ //
+ // THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+ // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ // PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+ // CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ // EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ // PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ // PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ //
+ // Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+ //
+ // ************************************************************************
+ //@HEADER
+ */
+
+#ifndef KOKKOS_PROFILING_C_INTERFACE_HPP
+#define KOKKOS_PROFILING_C_INTERFACE_HPP
+
+#ifdef __cplusplus
+#include <cstddef>
+#include <cstdint>
+#else
+#include <stddef.h>
+#include <stdint.h>
+#endif
+
+#define KOKKOSP_INTERFACE_VERSION 20191080
+
+struct Kokkos_Profiling_KokkosPDeviceInfo {
+  size_t deviceID;
+};
+
+
+struct Kokkos_Profiling_SpaceHandle {
+  const char* name;
+};
+
+union Kokkos_Tuning_VariableValue_ValueUnion {
+  bool bool_value;
+  int int_value;
+  double double_value;
+  const char* string_value;
+};
+
+struct Kokkos_Tuning_VariableValue {
+ size_t id;
+ union Kokkos_Tuning_VariableValue_ValueUnion value;
+};
+
+
+struct Kokkos_Tuning_ValueSet {
+  int size;
+  struct Kokkos_Tuning_VariableValue* values;
+};
+
+struct Kokkos_Tuning_ValueRange {
+  Kokkos_Tuning_VariableValue lower;
+  Kokkos_Tuning_VariableValue upper;
+  bool openLower;
+  bool openUpper;
+};
+
+enum Kokkos_Tuning_VariableInfo_ValueType {
+    kokkos_value_floating_point, // TODO DZP: single and double? One or the other?
+    kokkos_value_integer,
+    kokkos_value_text,
+    kokkos_value_boolean
+};
+
+enum Kokkos_Tuning_VariableInfo_StatisticalCategory {
+  kokkos_value_categorical, // unordered distinct objects
+  kokkos_value_ordinal,     // ordered distinct objects
+  kokkos_value_interval,    // ordered distinct objects for which distance matters
+  kokkos_value_ratio        // ordered distinct objects for which distance matters, division matters, and the concept of zero exists
+};
+
+enum Kokkos_Tuning_VariableInfo_CandidateValueType {
+  kokkos_value_set,        // I am one of [2,3,4,5]
+  kokkos_value_range,      // I am somewhere in [2,12)
+  kokkos_value_unbounded   // I am [text/int/float], but we don't know at declaration time what values are appropriate. Only valid for Context Variables
+  // TODO DZP: not handled: 1 + 3x, sets of ranges, range with hole (zero). Do these matter?
+};
+
+union Kokkos_Tuning_VariableInfo_SetOrRange {
+  struct Kokkos_Tuning_ValueSet set;
+  struct Kokkos_Tuning_ValueRange range;
+};
+
+struct Kokkos_Tuning_VariableInfo {
+  enum Kokkos_Tuning_VariableInfo_ValueType type;
+  enum Kokkos_Tuning_VariableInfo_StatisticalCategory category;
+  enum Kokkos_Tuning_VariableInfo_CandidateValueType valueQuantity;
+};
+
+typedef void (*Kokkos_Profiling_initFunction)(const int, const uint64_t, const uint32_t,
+                            Kokkos_Profiling_KokkosPDeviceInfo*);
+typedef void (*Kokkos_Profiling_finalizeFunction)();
+typedef void (*Kokkos_Profiling_beginFunction)(const char*, const uint32_t, uint64_t*);
+typedef void (*Kokkos_Profiling_endFunction)(uint64_t);
+
+typedef void (*Kokkos_Profiling_pushFunction)(const char*);
+typedef void (*Kokkos_Profiling_popFunction)();
+
+typedef void (*Kokkos_Profiling_allocateDataFunction)(const Kokkos_Profiling_SpaceHandle, const char*,
+                                     const void*, const uint64_t);
+typedef void (*Kokkos_Profiling_deallocateDataFunction)(const Kokkos_Profiling_SpaceHandle, const char*,
+                                       const void*, const uint64_t);
+
+typedef void (*Kokkos_Profiling_createProfileSectionFunction)(const char*, uint32_t*);
+typedef void (*Kokkos_Profiling_startProfileSectionFunction)(const uint32_t);
+typedef void (*Kokkos_Profiling_stopProfileSectionFunction)(const uint32_t);
+typedef void (*Kokkos_Profiling_destroyProfileSectionFunction)(const uint32_t);
+
+typedef void (*Kokkos_Profiling_profileEventFunction)(const char*);
+
+typedef void (*Kokkos_Profiling_beginDeepCopyFunction)(Kokkos_Profiling_SpaceHandle, const char*, const void*,
+                                      Kokkos_Profiling_SpaceHandle, const char*, const void*,
+                                      uint64_t);
+typedef void (*Kokkos_Profiling_endDeepCopyFunction)();
+
+typedef void (*Kokkos_Tuning_tuningVariableDeclarationFunction)(const char*, const size_t, Kokkos_Tuning_VariableInfo info); 
+typedef void (*Kokkos_Tuning_contextVariableDeclarationFunction)(const char*, const size_t, Kokkos_Tuning_VariableInfo info, Kokkos_Tuning_VariableInfo_SetOrRange); 
+typedef void (*Kokkos_Tuning_tuningVariableValueFunction)(const size_t, const size_t, const size_t*, const Kokkos_Tuning_VariableValue*, const size_t count, const size_t* uniqIds, Kokkos_Tuning_VariableValue*, Kokkos_Tuning_VariableInfo_SetOrRange*);
+typedef void (*Kokkos_Tuning_contextVariableValueFunction)(const size_t contextId, const size_t count, const size_t* uniqIds, Kokkos_Tuning_VariableValue* values);
+typedef void (*Kokkos_Tuning_contextEndFunction)(const size_t);
+
+
+#endif // KOKKOS_PROFILING_C_INTERFACE_HPP
---
+++ include/impl/Kokkos_Profiling_Interface.hpp
@@ -0,0 +1,178 @@
+/*
+ //@HEADER
+ // ************************************************************************
+ //
+ //                        Kokkos v. 2.0
+ //              Copyright (2014) Sandia Corporation
+ //
+ // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+ // the U.S. Government retains certain rights in this software.
+ //
+ // Redistribution and use in source and binary forms, with or without
+ // modification, are permitted provided that the following conditions are
+ // met:
+ //
+ // 1. Redistributions of source code must retain the above copyright
+ // notice, this list of conditions and the following disclaimer.
+ //
+ // 2. Redistributions in binary form must reproduce the above copyright
+ // notice, this list of conditions and the following disclaimer in the
+ // documentation and/or other materials provided with the distribution.
+ //
+ // 3. Neither the name of the Corporation nor the names of the
+ // contributors may be used to endorse or promote products derived from
+ // this software without specific prior written permission.
+ //
+ // THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+ // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ // PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+ // CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ // EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ // PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ // PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ //
+ // Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+ //
+ // ************************************************************************
+ //@HEADER
+ */
+
+#ifndef KOKKOSP_INTERFACE_HPP
+#define KOKKOSP_INTERFACE_HPP
+
+#include <cinttypes>
+#include <cstddef>
+#include <string>
+
+#include <iostream>
+#include <cstdlib>
+
+#include <impl/Kokkos_Profiling_DeviceInfo.hpp>
+#include <impl/Kokkos_Profiling_DeviceInfo.hpp>
+#include <impl/Kokkos_Profiling_C_Interface.h>
+#include <impl/Kokkos_Profiling_C_Interface.h>
+#define KOKKOS_ENABLE_TUNING  // TODO DZP: this needs to be a proper build
+                              // system option
+namespace Kokkos {
+namespace Profiling {
+
+using SpaceHandle = Kokkos_Profiling_SpaceHandle;
+
+}  // end namespace Profiling
+
+namespace Tuning {
+
+using ValueSet            = Kokkos_Tuning_ValueSet;
+using ValueRange          = Kokkos_Tuning_ValueRange;
+using StatisticalCategory = Kokkos_Tuning_VariableInfo_StatisticalCategory;
+using ValueType           = Kokkos_Tuning_VariableInfo_ValueType;
+using CandidateValueType  = Kokkos_Tuning_VariableInfo_CandidateValueType;
+using SetOrRange          = Kokkos_Tuning_VariableInfo_SetOrRange;
+using VariableInfo        = Kokkos_Tuning_VariableInfo;
+// TODO DZP: VariableInfo subclasses to automate some of this
+
+using VariableValue = Kokkos_Tuning_VariableValue;
+
+VariableValue make_variable_value(size_t id, bool val);
+VariableValue make_variable_value(size_t id, int val);
+VariableValue make_variable_value(size_t id, double val);
+VariableValue make_variable_value(size_t id, const char* val);
+
+}  // end namespace Tuning
+
+namespace Profiling {
+
+using initFunction           = Kokkos_Profiling_initFunction;
+using finalizeFunction       = Kokkos_Profiling_finalizeFunction;
+using beginFunction          = Kokkos_Profiling_beginFunction;
+using endFunction            = Kokkos_Profiling_endFunction;
+using pushFunction           = Kokkos_Profiling_pushFunction;
+using popFunction            = Kokkos_Profiling_popFunction;
+using allocateDataFunction   = Kokkos_Profiling_allocateDataFunction;
+using deallocateDataFunction = Kokkos_Profiling_deallocateDataFunction;
+using createProfileSectionFunction =
+    Kokkos_Profiling_createProfileSectionFunction;
+using startProfileSectionFunction =
+    Kokkos_Profiling_startProfileSectionFunction;
+using stopProfileSectionFunction = Kokkos_Profiling_stopProfileSectionFunction;
+using destroyProfileSectionFunction =
+    Kokkos_Profiling_destroyProfileSectionFunction;
+using profileEventFunction  = Kokkos_Profiling_profileEventFunction;
+using beginDeepCopyFunction = Kokkos_Profiling_beginDeepCopyFunction;
+using endDeepCopyFunction   = Kokkos_Profiling_endDeepCopyFunction;
+
+}  // end namespace Profiling
+
+namespace Tuning {
+using tuningVariableDeclarationFunction =
+    Kokkos_Tuning_tuningVariableDeclarationFunction;
+using contextVariableDeclarationFunction =
+    Kokkos_Tuning_contextVariableDeclarationFunction;
+using tuningVariableValueFunction  = Kokkos_Tuning_tuningVariableValueFunction;
+using contextVariableValueFunction = Kokkos_Tuning_contextVariableValueFunction;
+using contextEndFunction           = Kokkos_Tuning_contextEndFunction;
+
+}  // end namespace Tuning
+
+}  // namespace Kokkos
+
+#if 0  // TODO DZP: reimplement or set on fire
+namespace Kokkos {
+namespace Profiling {
+
+struct SpaceHandle {
+  SpaceHandle(const char* space_name);
+  char name[64];
+};
+
+bool profileLibraryLoaded();
+
+void beginParallelFor(const std::string&, const uint32_t, uint64_t*);
+void endParallelFor(const uint64_t);
+void beginParallelScan(const std::string&, const uint32_t, uint64_t*);
+void endParallelScan(const uint64_t);
+void beginParallelReduce(const std::string&, const uint32_t, uint64_t*);
+void endParallelReduce(const uint64_t);
+
+void pushRegion(const std::string&);
+void popRegion();
+void createProfileSection(const std::string&, uint32_t*);
+void startSection(const uint32_t);
+void stopSection(const uint32_t);
+void destroyProfileSection(const uint32_t);
+
+void markEvent(const std::string&);
+
+void allocateData(const SpaceHandle, const std::string, const void*,
+                  const uint64_t);
+void deallocateData(const SpaceHandle, const std::string, const void*,
+                    const uint64_t);
+
+void beginDeepCopy(const SpaceHandle, const std::string, const void*,
+                   const SpaceHandle, const std::string, const void*,
+                   const uint64_t);
+void endDeepCopy();
+
+void initialize();
+void finalize();
+
+} // end namespace Profiling
+namespace Tuning  {
+void declareTuningVariable(const std::string& variableName, int uniqID, VariableInfo info); 
+
+void declareContextVariable(const std::string& variableName, int uniqID, VariableInfo info); 
+
+void declareContextVariableValues(int contextId, int count, int* uniqIds, VariableValue* values);
+
+void endContext(int contextId);
+
+void requestTuningVariableValues(int count, int* uniqIds, VariableValue* values);
+
+} // end namespace Tuning
+} // end namespace Kokkos
+#endif
+#endif
