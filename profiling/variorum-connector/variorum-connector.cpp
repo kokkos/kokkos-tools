b9f72d5e6656511bfd44c378a1f4609a9aa60304
Zachary S. Frye <frye7@llnl.gov>
2020-08-20 13:47:19 -0700

    Made recommended changes to variorum-connector.cpp

--- profiling/variorum-connector/variorum-connector.cpp
+++ profiling/variorum-connector/variorum-connector.cpp
@@ -82,19 +82,13 @@ static endFunction endForCallee = NULL;
 static endFunction endScanCallee = NULL;
 static endFunction endReduceCallee = NULL;
 
-//variables for simple timer
-time_t start_time;
+//Function: variorum_print_power_call()
+//Description: function that will call print power and handle the execution errors
+//Pre: None
+//Post: Will print an error message if variorum print power fails. No return value. 
 
-
-extern "C" void kokkosp_init_library(const int loadSeq,
-	const uint64_t interfaceVer, const uint32_t devInfoCount, void* deviceInfo) {
-    
+void variorum_print_power_call() {
     int ret;
-    //Simple timer code to keep track of the general amount of time the application ran for.
-    time(&start_time);
-    std::cout << "Start Time: " << start_time << std::endl;
-
-    //print variorum power when Kokkos is initialized called
     //Pre: No Arguments required for print power
     //Post: An integer value that represents the return success/error code
     //Description: variorum_print_power prints out the current architecture specific power usage measurements
@@ -105,61 +99,59 @@ extern "C" void kokkosp_init_library(const int loadSeq,
     }
 }
 
-extern "C" void kokkosp_finalize_library() {
+//variables for simple timer
+time_t start_time;
 
+
+extern "C" void kokkosp_init_library(const int loadSeq,
+	const uint64_t interfaceVer, const uint32_t devInfoCount, void* deviceInfo) {
+    
     int ret;
-    //print variorum power when kokkos is finalized is called
+    //Simple timer code to keep track of the general amount of time the application ran for.
+    time(&start_time);
+    std::cout << "Start Time: " << start_time << "\n";
+
+    //print variorum power when Kokkos is initialized called
 	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
-    time_t total_time;
+    variorum_print_power_call();
+}
+
+extern "C" void kokkosp_finalize_library() {
+    //print variorum power when kokkos finalize is called 
+    variorum_print_power_call();
     time_t end_time;
     time(&end_time);
     std::cout << "End Time: " << end_time << "\nStart Time: " << start_time << std::endl;
-    total_time = end_time - start_time;
+    time_t total_time = end_time - start_time;
 
-    std::cout << "The kokkos library was alive for " << total_time << " seconds." << std::endl;
+    std::cout << "The kokkos library was alive for " << total_time << " seconds." << "\n";
 }
 
 extern "C" void kokkosp_begin_parallel_for(const char* name, const uint32_t devID, uint64_t* kID) {
 
     int ret;
     //print variorum power whenever parallelfor is called
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+	variorum_print_power_call();
 }
 
 extern "C" void kokkosp_end_parallel_for(const uint64_t kID) {
 
     //print variorum power ever time a parallel for has finished executing 
-    int ret;
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+    variorum_print_power_call();
 }
 
 extern "C" void kokkosp_begin_parallel_scan(const char* name, const uint32_t devID, uint64_t* kID) {
 	
     int ret;
     //print variorum power when parallel scan begins 
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+	variorum_print_power_call();
 }
 
 extern "C" void kokkosp_end_parallel_scan(const uint64_t kID) {
     
     int ret;
     //print power when a parallel scan completes
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+	variorum_print_power_call();
 	
 }
 
@@ -167,20 +159,14 @@ extern "C" void kokkosp_begin_parallel_reduce(const char* name, const uint32_t d
     
     int ret;
     //print power when a parallel reduce begins
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+	variorum_print_power_call();
 }
 
 extern "C" void kokkosp_end_parallel_reduce(const uint64_t kID) {
     int ret;
 
     //print power when a parallel reduce completes 
-	ret = variorum_print_power();
-    if (ret != 0) {
-        printf("Print power failed!\n");
-    }
+	variorum_print_power_call();
 }
 
 
