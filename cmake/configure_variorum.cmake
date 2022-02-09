cd80433cc7284450ee181eed490889e96d5c9670
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-02-09 15:09:03 +0100

    Move Variorum config script to cmake folder

--- cmake/configure_variorum.cmake
+++ cmake/configure_variorum.cmake
@@ -2,16 +2,16 @@
 
 set(KOKKOSTOOLS_HAS_VARIORUM OFF)
 
-# Set Variorum_DIR for find_package() based on VARIORUM_ROOT (CMake or environment variable)
-set(MSG_NOTFOUND "set Variorum_DIR CMake variable or VARIORUM_ROOT environment variable to build Variorum connector")
-if(NOT DEFINED Variorum_DIR)
+# Set Variorum_ROOT for find_package() based on VARIORUM_ROOT (CMake or environment variable)
+set(MSG_NOTFOUND "set Variorum_ROOT CMake variable or VARIORUM_ROOT environment variable to build Variorum connector")
+if(NOT DEFINED Variorum_ROOT)
   if(DEFINED ENV{VARIORUM_ROOT})
     set(Variorum_ROOT $ENV{VARIORUM_ROOT})
     set(MSG_NOTFOUND "check VARIORUM_ROOT environment variable ($ENV{VARIORUM_ROOT})")
   endif()
-  set(Variorum_DIR ${VARIORUM_ROOT})
+  set(Variorum_ROOT ${VARIORUM_ROOT})
 else()
-  set(MSG_NOTFOUND "check Variorum_DIR (${Variorum_DIR})")
+  set(MSG_NOTFOUND "check Variorum_ROOT (${Variorum_ROOT})")
 endif()
 
 find_package(Variorum QUIET)
