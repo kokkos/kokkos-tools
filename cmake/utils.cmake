08da30886e817b6c0c44f434023ec88e321676ac
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-05-16 18:52:02 +0200

    Fix replicated BUILD_SHARED_LIBS effects

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,5 +1,5 @@
 function(kp_add_library TARGET)
-  add_library(${TARGET} ${KOKKOSTOOLS_LIBRARY_MODE} ${ARGN}) # SOURCES = ${ARGN}
+  add_library(${TARGET} ${ARGN}) # SOURCES = ${ARGN}
 
   # add this library to the list of profilers linked to single library
   list(APPEND SINGLELIB_PROFILERS ${TARGET})
