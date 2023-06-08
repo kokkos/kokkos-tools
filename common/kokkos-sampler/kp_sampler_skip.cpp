85b4d1a7b94ecdcee5081f585928b0e496848e57
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2023-06-08 16:42:59 -0700

    formatted fix to kp sampler

--- common/kokkos-sampler/kp_sampler_skip.cpp
+++ common/kokkos-sampler/kp_sampler_skip.cpp
@@ -61,6 +61,12 @@ void invoke_ktools_fence(uint32_t devID) {
 
 void kokkosp_provide_tool_programming_interface(
     uint32_t num_funcs, Kokkos_Tools_ToolProgrammingInterface* funcsFromTPI) {
+  if (!num_funcs) {
+    if (tool_verbosity > 0)
+      printf(
+          "KokkosP: Note: Number of functions in Tools Programming Interface "
+          "is 0!\n");
+  }
   tpi_funcs = *funcsFromTPI;
 }
 
