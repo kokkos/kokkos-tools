f15f7e7c57f0d806d9664c7c623d469ed62e8a8c
blegouix <stilynx51@gmail.com>
2024-01-30 14:02:33 +0100

    merge kp-kernels-timers

--- profiling/simple-kernel-timer/kp_kernel_timer.cpp
+++ profiling/simple-kernel-timer/kp_kernel_timer.cpp
@@ -15,6 +15,7 @@
 //@HEADER
 
 #include <vector>
+#include <algorithm>
 #include <string>
 #include <iostream>
 #include <unistd.h>
@@ -24,6 +25,10 @@
 
 namespace KokkosTools {
 namespace KernelTimer {
+
+bool is_region(KernelPerformanceInfo const& kp) {
+  return kp.getKernelType() == REGION;
+}
 
 void kokkosp_init_library(const int loadSeq, const uint64_t interfaceVer,
                           const uint32_t /*devInfoCount*/,
@@ -50,25 +55,94 @@ void kokkosp_init_library(const int loadSeq, const uint64_t interfaceVer,
 }
 
 void kokkosp_finalize_library() {
-  double finishTime = seconds();
+  const char* kp_kernel_timer_json_raw = getenv("KP_KERNEL_TIMER_JSON");
+  const bool kp_kernel_timer_json =
+      strcmp(kp_kernel_timer_json_raw, "1") == 0 ||
+      strcmp(kp_kernel_timer_json_raw, "true") == 0 ||
+      strcmp(kp_kernel_timer_json_raw, "True") == 0;
+
+  double finishTime  = seconds();
+  double kernelTimes = 0;
+
+  char* mpi_rank = getenv("OMPI_COMM_WORLD_RANK");
 
   char* hostname = (char*)malloc(sizeof(char) * 256);
   gethostname(hostname, 256);
 
   char* fileOutput = (char*)malloc(sizeof(char) * 256);
-  snprintf(fileOutput, 256, "%s-%d.dat", hostname, (int)getpid());
+  snprintf(fileOutput, 256, "%s-%d-%s.%s", hostname, (int)getpid(),
+           (NULL == mpi_rank) ? "0" : mpi_rank,
+           kp_kernel_timer_json ? "json" : "dat");
 
   free(hostname);
   FILE* output_data = fopen(fileOutput, "wb");
 
-  const double totalExecuteTime = (finishTime - initTime);
-  fwrite(&totalExecuteTime, sizeof(totalExecuteTime), 1, output_data);
-
   std::vector<KernelPerformanceInfo*> kernelList;
 
-  for (auto kernel_itr = count_map.begin(); kernel_itr != count_map.end();
-       kernel_itr++) {
-    kernel_itr->second->writeToBinaryFile(output_data);
+  const double totalExecuteTime = (finishTime - initTime);
+  if (!kp_kernel_timer_json) {
+    fwrite(&totalExecuteTime, sizeof(totalExecuteTime), 1, output_data);
+
+    for (auto kernel_itr = count_map.begin(); kernel_itr != count_map.end();
+         kernel_itr++) {
+      kernel_itr->second->writeToBinaryFile(output_data);
+    }
+  } else {
+    for (auto kernel_itr = count_map.begin(); kernel_itr != count_map.end();
+         kernel_itr++) {
+      kernelList.push_back(kernel_itr->second);
+      kernelTimes += kernel_itr->second->getTime();
+    }
+
+    std::sort(kernelList.begin(), kernelList.end(),
+              compareKernelPerformanceInfo);
+
+    fprintf(output_data, "{\n\"kokkos-kernel-data\" : {\n");
+    fprintf(output_data, "    \"mpi-rank\"               : %s,\n",
+            (NULL == mpi_rank) ? "0" : mpi_rank);
+    fprintf(output_data, "    \"total-app-time\"         : %10.3f,\n",
+            totalExecuteTime);
+    fprintf(output_data, "    \"total-kernel-times\"     : %10.3f,\n",
+            kernelTimes);
+    fprintf(output_data, "    \"total-non-kernel-times\" : %10.3f,\n",
+            (totalExecuteTime - kernelTimes));
+
+    const double percentKokkos = (kernelTimes / totalExecuteTime) * 100.0;
+    fprintf(output_data, "    \"percent-in-kernels\"     : %6.2f,\n",
+            percentKokkos);
+    fprintf(output_data, "    \"unique-kernel-calls\"    : %22llu,\n",
+            (unsigned long long)count_map.size());
+    fprintf(output_data, "\n");
+
+    fprintf(output_data, "    \"region-perf-info\"       : [\n");
+
+#define KERNEL_INFO_INDENT "       "
+
+    bool print_comma = false;
+    for (auto const& kernel : count_map) {
+      if (!is_region(*std::get<1>(kernel))) continue;
+      if (print_comma) fprintf(output_data, ",\n");
+      kernel.second->writeToJSONFile(output_data, KERNEL_INFO_INDENT);
+      print_comma = true;
+    }
+
+    fprintf(output_data, "\n");
+    fprintf(output_data, "    ],\n");
+
+    fprintf(output_data, "    \"kernel-perf-info\"       : [\n");
+
+    print_comma = false;
+    for (auto const& kernel : count_map) {
+      if (is_region(*std::get<1>(kernel))) continue;
+      if (print_comma) fprintf(output_data, ",\n");
+      kernel.second->writeToJSONFile(output_data, KERNEL_INFO_INDENT);
+      print_comma = true;
+    }
+
+    fprintf(output_data, "\n");
+    fprintf(output_data, "    ]\n");
+
+    fprintf(output_data, "}\n}");
   }
 
   fclose(output_data);
