49ffd617d140271536b28a80ef9a7f4ae037b74b
Ethan Puyaubreau <ethan.puy13@gmail.com>
2026-03-05 12:36:01 +0100

    Energy profiling tools: Add Daemon class for periodic task execution (#300)
    * Add Daemon class for managing periodic task execution
    
    ---------
    
    Co-authored-by: Ethan Puyaubreau <ethan.puyaubreau@gmail.com>
    Co-authored-by: Jakob Bludau <bludauj@ornl.gov>
    Co-authored-by: Maarten Arnst <maarten.arnst@ulg.ac.be>
    Co-authored-by: Andrey Prokopenko <andrey.prok@gmail.com>

---
+++ profiling/energy-profiler/daemon.cpp
@@ -0,0 +1,43 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#include "daemon.hpp"
+#include <stdexcept>
+#include <thread>
+
+namespace KokkosTools::EnergyProfiler {
+void Daemon::start() {
+  if (!running_) {
+    running_ = true;
+    thread_  = std::thread(&Daemon::run, this);
+  }
+}
+
+void Daemon::stop() {
+  if (running_) {
+    running_ = false;
+    thread_.join();
+  }
+}
+
+void Daemon::run() {
+  while (running_) {
+    auto next_run = std::chrono::high_resolution_clock::now() + interval_;
+    func_();
+    std::this_thread::sleep_until(next_run);
+  }
+}
+}  // namespace KokkosTools::EnergyProfiler
