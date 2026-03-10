20a0281fac07765aa76946cc37271f448e770b63
Daniel Arndt <arndtd@ornl.gov>
2026-03-10 10:27:30 -0600

    Refactor more

--- profiling/memory-events/kp_memory_events.cpp
+++ profiling/memory-events/kp_memory_events.cpp
@@ -59,43 +59,43 @@ void kokkosp_finalize_library() {
 
   int pid = getpid();
 
+  // ---- Memory event log ----
   std::string fileOutput = hostname + "-" + std::to_string(pid) + ".mem_events";
-
   std::ofstream ofile(fileOutput, std::ios::binary);
 
   ofile << "# Memory Events\n";
-  ofile << "# Time     Ptr                  Size        MemSpace      Op"
-        << "         Name\n";
+  ofile << "# Time     Ptr                  Size        MemSpace      Op       "
+           "  Name\n";
 
   for (const auto& e : events) {
-    e.print_record(ofile);  // assuming you overload to accept std::ostream
+    e.print_record(ofile);
   }
 
-  ofile.close();
+  // ---- Per memory space usage ----
+  for (int s = 0; s < num_spaces; ++s) {
+    std::string fileName = hostname + "-" + std::to_string(pid) + "-" +
+                           space_name[s] + ".memspace_usage";
 
-  for (int s = 0; s < num_spaces; s++) {
-    char* fileOutput = (char*)malloc(sizeof(char) * 256);
-    [[maybe_unused]] auto written_bytes =
-        snprintf(fileOutput, 256, "%s-%d-%s.memspace_usage", hostname.c_str(),
-                 pid, space_name[s]);
+    std::ofstream spaceFile(fileName, std::ios::binary);
 
-    FILE* ofile = fopen(fileOutput, "wb");
-    free(fileOutput);
+    spaceFile << "# Space " << space_name[s] << "\n";
+    spaceFile
+        << "# Time(s)  Size(MB)   HighWater(MB)   HighWater-Process(MB)\n";
 
-    fprintf(ofile, "# Space %s\n", space_name[s]);
-    fprintf(ofile,
-            "# Time(s)  Size(MB)   HighWater(MB)   HighWater-Process(MB)\n");
     uint64_t maxvalue = 0;
-    for (unsigned int i = 0; i < space_size_track[s].size(); i++) {
-      if (std::get<1>(space_size_track[s][i]) > maxvalue)
-        maxvalue = std::get<1>(space_size_track[s][i]);
-      fprintf(ofile, "%lf %.1lf %.1lf %.1lf\n",
-              std::get<0>(space_size_track[s][i]),
-              1.0 * std::get<1>(space_size_track[s][i]) / 1024 / 1024,
-              1.0 * maxvalue / 1024 / 1024,
-              1.0 * std::get<2>(space_size_track[s][i]) / 1024 / 1024);
+
+    for (const auto& entry : space_size_track[s]) {
+      double time         = std::get<0>(entry);
+      uint64_t size       = std::get<1>(entry);
+      uint64_t process_hw = std::get<2>(entry);
+
+      if (size > maxvalue) maxvalue = size;
+
+      spaceFile << time << " " << std::fixed << std::setprecision(1)
+                << (size / 1024.0 / 1024.0) << " "
+                << (maxvalue / 1024.0 / 1024.0) << " "
+                << (process_hw / 1024.0 / 1024.0) << "\n";
     }
-    fclose(ofile);
   }
 }
 
