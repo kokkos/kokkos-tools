12c98323183d5a466061424c45dd3b7d70cc3d08
Daniel Arndt <arndtd@ornl.gov>
2026-03-10 13:16:56 -0600

    Fix kp_memory_usage

--- profiling/memory-usage/kp_memory_usage.cpp
+++ profiling/memory-usage/kp_memory_usage.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <atomic>
 #include <mutex>
+#include <fstream>
 
 #include <sys/resource.h>
 #include <unistd.h>
@@ -45,34 +46,40 @@ void kokkosp_init_library(const int /*loadSeq*/,
 }
 
 void kokkosp_finalize_library() {
-  char* hostname = (char*)malloc(sizeof(char) * 256);
-  gethostname(hostname, 256);
+  std::array<char, 256> hostname_buf{};
+  gethostname(hostname_buf.data(), hostname_buf.size());
+  std::string hostname(hostname_buf.data());
+
   int pid = getpid();
 
-  for (int s = 0; s < num_spaces; s++) {
-    char* fileOutput = (char*)malloc(sizeof(char) * 256);
-    snprintf(fileOutput, 256, "%s-%d-%s.memspace_usage", hostname, pid,
-             space_name[s]);
+  std::string fileOutput = hostname + "-" + std::to_string(pid) + ".mem_events";
+  std::ofstream ofile(fileOutput, std::ios::binary);
 
-    FILE* ofile = fopen(fileOutput, "wb");
-    free(fileOutput);
+  for (int s = 0; s < num_spaces; ++s) {
+    std::string fileName = hostname + "-" + std::to_string(pid) + "-" +
+                           space_name[s] + ".memspace_usage";
 
-    fprintf(ofile, "# Space %s\n", space_name[s]);
-    fprintf(ofile,
-            "# Time(s)  Size(MB)   HighWater(MB)   HighWater-Process(MB)\n");
+    std::ofstream spaceFile(fileName, std::ios::binary);
+
+    spaceFile << "# Space " << space_name[s] << "\n";
+    spaceFile
+        << "# Time(s)  Size(MB)   HighWater(MB)   HighWater-Process(MB)\n";
+
     uint64_t maxvalue = 0;
-    for (unsigned int i = 0; i < space_size_track[s].size(); i++) {
-      if (std::get<1>(space_size_track[s][i]) > maxvalue)
-        maxvalue = std::get<1>(space_size_track[s][i]);
-      fprintf(ofile, "%lf %.1lf %.1lf %.1lf\n",
-              std::get<0>(space_size_track[s][i]),
-              1.0 * std::get<1>(space_size_track[s][i]) / 1024 / 1024,
-              1.0 * maxvalue / 1024 / 1024,
-              1.0 * std::get<2>(space_size_track[s][i]) / 1024 / 1024);
+
+    for (const auto& entry : space_size_track[s]) {
+      double time         = std::get<0>(entry);
+      uint64_t size       = std::get<1>(entry);
+      uint64_t process_hw = std::get<2>(entry);
+
+      if (size > maxvalue) maxvalue = size;
+
+      spaceFile << time << " " << std::fixed << std::setprecision(1)
+                << (size / 1024.0 / 1024.0) << " "
+                << (maxvalue / 1024.0 / 1024.0) << " "
+                << (process_hw / 1024.0 / 1024.0) << "\n";
     }
-    fclose(ofile);
   }
-  free(hostname);
 }
 
 void kokkosp_allocate_data(const SpaceHandle space, const char* /*label*/,
