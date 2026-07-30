afced73d5de009eb2bb5a8d6f902a37d8e8e83ce
Victor Brunini <vebruni@sandia.gov>
2026-07-30 12:43:06 -0400

    SpaceTimeStack: Account for MPI imbalance when applying printing threshold and add test (#284)
    * SpaceTimeStack: Account for MPI imbalance when applying printing threshold.
    
    So that entries with a low average time but high imbalance are reported
    since they are often the cause of significant performance problems due
    to load imbalance.
    
    * SpaceTimeStack: Fix format.
    
    * SpaceTimeStack: Fix failure when MPI not initialized.
    
    * SpaceTimeStack: Add test for thresholding including imbalance.
    
    * SpaceTimeStack: Format.
    
    * SpaceTimeStack: Fix test compile & delete extra file.
    
    * SpaceTimeStack: Address review comments.
    
    * SpaceTimeStack: Format.
    
    * Allow MPI to run as root
    
    * Update tests/CMakeLists.txt
    
    Remove debugging message.
    
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    
    * Update tests/space-time-stack/test_threshold_with_imbalance.cpp
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    * Update tests/space-time-stack/test_threshold_with_imbalance.cpp
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    * Update tests/CMakeLists.txt
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    * Update tests/space-time-stack/test_threshold_with_imbalance.cpp
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    * Silence -Wrestrict on deep_copy test
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    * clang-format
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    
    ---------
    
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    Co-authored-by: Jan Ciesko <jan.ciesko@gmail.com>

---
+++ tests/space-time-stack/test_threshold_with_imbalance.cpp
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
+
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "Kokkos_Core.hpp"
+#include "Kokkos_Profiling_ScopedRegion.hpp"
+#include "mpi.h"
+
+struct Tester {
+  explicit Tester() {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // The threshold for filtering out lines from the stack is 0.1% of the total
+    // runtime. Do a 1s sleep on all ranks, then a 1.5 ms sleep on only rank 1.
+    // If the threshold is applied based on the average time across ranks the
+    // second region will be filtered out, but if it is based on the max time
+    // then it will be included (which is the desired behavior so that we see
+    // things which are slow only on a subset of ranks in cases like a coarse
+    // solve in a multigrid method that only runs on a subset of the ranks).
+    {
+      Kokkos::Profiling::ScopedRegion all_ranks_region("all_ranks_region");
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    {
+      Kokkos::Profiling::ScopedRegion rank_1_region("rank_1_region");
+      if (rank == 1) {
+        std::this_thread::sleep_for(std::chrono::microseconds(1500));
+      }
+    }
+  }
+};
+
+static const std::vector<std::string> matchers{
+    "[0-9.e]+ sec [0-9.]+% [0-9.]+% 0.[0-9]+% 100.0% 0.00e\\+00 1 "
+    "all_ranks_region \\[region\\]",
+    "[0-9.e]+ sec [0-9.]+% [0-9.]+% [1-9][0-9.]+% 100.0% 0.00e\\+00 1 "
+    "rank_1_region \\[region\\]",
+};
+
+/**
+ * @test This test checks that the tool reports imbalanced MPI regions that
+ * would be filtered out when using average time alone.
+ */
+TEST(SpaceTimeStackTest, threshold_with_imbalance) {
+  MPI_Init(nullptr, nullptr);
+  int comm_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+  ASSERT_EQ(2, comm_size) << " test requires exactly 2 MPI ranks.";
+
+  //! Initialize @c Kokkos.
+  Kokkos::initialize();
+
+  //! Redirect output for later analysis.
+  std::cout.flush();
+  std::ostringstream output;
+  std::streambuf* coutbuf = std::cout.rdbuf(output.rdbuf());
+
+  Tester tester;
+
+  //! Finalize @c Kokkos.
+  Kokkos::finalize();
+
+  //! Restore output buffer.
+  std::cout.flush();
+  std::cout.rdbuf(coutbuf);
+  std::cout << output.str() << std::endl;
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  //! Analyze test output.
+  if (rank == 0) {
+    for (const auto& matcher : matchers) {
+      EXPECT_THAT(output.str(), ::testing::ContainsRegex(matcher));
+    }
+  }
+  MPI_Finalize();
+}
